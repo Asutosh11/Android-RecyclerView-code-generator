e134ecf3221b8157c954675fe19620d4af87be99
Asutosh Panda <Asutosh11@users.noreply.github.com>
2017-07-20 12:22:32 +0530

    footer text change

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
 <footer>
     <center>
 
-        For the love of coding and open source. &nbsp; &nbsp;
+        Proudly open source. &nbsp; &nbsp;
         <a href="https://github.com/Asutosh11" class='footer-link'>Work by Asutosh Panda</a>
 
     </center>
